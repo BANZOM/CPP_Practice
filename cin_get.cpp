2f5b77bbd736e767a6961294099dae822771faf4
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-05-19 18:52:04 +0530

    ABLE TO GET THE VALUE OF TAB, SPACE, ENTER;

---
+++ cin_get.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+cin.get     : ABLE TO GET THE VALUE OF TAB, SPACE, ENTER;
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    a= cin.get();
+    
+    cout << "the VALUE of a is : " << a << endl ;
+
+    return 0;
+}
