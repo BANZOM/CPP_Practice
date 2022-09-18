32acd4c4d066e43726110831e56416b9348fa843
Aditya Panwar <aditya.panwar.2208@gmail.com>
2022-09-18 08:28:30 +0530

    added

---
+++ bitwise_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "a & b {and} = " << a & b << endl;
+    cout << "a | b {or} = " << a | b << endl;
+    cout << "~a {not} = " << ~a << endl;
+    cout << "~b {not} = " << ~b << endl;
+    cout << "a ^ b {xor} = " << a ^ b << endl;
+
+    // cout << "The result is: " << a << " " << b;
+}
