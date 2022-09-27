280eef46b1dd11764e39181fa4e5676730a092f7
Aditya Panwar <aditya.panwar.2208@gmail.com>
2022-09-27 22:15:21 +0530

    completed

--- decimal2binart.cpp
+++ decimal2binart.cpp
@@ -13,8 +13,8 @@ int main()
     int i = 0;
     while (n != 0)
     {
-        int bit = n & 1; // and of even gives 0 and odd gives 1
-        answer += (bit * pow(10, i));
+        int bit = n & 1;
+        answer = (bit * pow(10, i)) + answer;
         n = n >> 1;
         i++;
     }
