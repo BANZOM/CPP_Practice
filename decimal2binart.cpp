81074b75b32750a1df41650fdf078ef525b09100
Aditya Panwar <aditya.panwar.2208@gmail.com>
2022-09-27 22:09:57 +0530

    ..

--- decimal2binart.cpp
+++ decimal2binart.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 int main()
 {
+    int n, answer = 0;
+    cout << "Enter the decimal number: ";
+    cin >> n;
 
+    // logic
+    int i = 0;
+    while (n != 0)
+    {
+        int bit = n & 1; // and of even gives 0 and odd gives 1
+        answer += (bit * pow(10, i));
+        n = n >> 1;
+        i++;
+    }
+
+    cout << "The binary form is : " << answer << endl;
     return 0;
 }
