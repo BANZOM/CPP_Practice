5d7bbe03a0926006c48ced199f9b8aa2f462a45f
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-02 15:05:39 +0530

    added

--- insertAtPosSLL.cpp
+++ insertAtPosSLL.cpp
@@ -1,22 +1,66 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-
-struct head{
+struct head
+{
     int data;
     struct head *next;
 };
 typedef struct head *NODE;
 
-NODE createNode(){
+NODE createNode()
+{
     NODE newNode;
-    newNode = (NODE) malloc(sizeof(struct head));
-    newNode->next=NULL;
+    newNode = (NODE)malloc(sizeof(struct head));
+    newNode->next = NULL;
     return newNode;
 }
 
-NODE insertAtpos(NODE first,int pos,int x)
+NODE insertAtpos(NODE first, int pos, int x)
 {
-    
-}
+    NODE n, node;
+    node = first;
+    n = createNode();
+    n->data = x;
+
+    // no. of nodes in a list
+    int count = 0;
+    while (node->next != NULL)
+    {
+        count++;
+        node = node->next;
+    }
+
+    if (node == NULL && pos == 1)
+    {
+        first = n;
+        return first;
+    }
+
+    else
+    {
+        if (pos == 1)
+        {
+            n->next = first->next;
+            first = n;
+        }
+
+        else
+        {
+            int tempCount = 0;
+            while (node != NULL)
+            {
+                tempCount++;
+                if (tempCount == pos)
+                {
+                    break;
+                }
+                node = node->next;
+            }
+
+            n->next = node->next;
+            node->next = n;
+        }
+        return first;
+    }
