e0694069053422203dfeb8c6fa8fbe349c4f3bfa
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-02 12:48:28 +0530

    modified:   codeTantra_LL02.cpp

--- codeTantra_LL02.cpp
+++ codeTantra_LL02.cpp
@@ -8,6 +8,49 @@ struct head{
 };
 typedef struct head *NODE;
 
+NODE createNode(){
+    NODE newNode;
+    newNode = (NODE) malloc(sizeof(struct head));
+    newNode->next=NULL;
+    return newNode;
+}
+
+NODE addNodes(NODE first,int x){
+    NODE node,c ;
+    
+    node=first;
+    c=createNode();
+    
+    if(node==NULL){
+        
+        first=c;
+        first->data=x;
+    }
+    else{
+        
+        while(node != NULL){
+            node=node->next;
+        }
+        node->next=c;
+        c->data=x;
+    }
+    return first;
+}
+
+
+void traverseList(NODE first){
+    NODE x;
+    x=first;
+    while(x !=NULL){
+        printf("%d-->",x->data);
+        x=x->next;
+    }
+    printf("NULL");
+}
+
+
+
+
 int main()
 {  
     NODE first = NULL;
@@ -17,7 +60,8 @@ int main()
     cin>>x;
     while(x != -1)
     {
-        first= addNodes(first,x);
+        first = addNodes(first,x);
+        // printf("\n**%d",first->data);
         cin>>x;
     }
     if(first== NULL)
