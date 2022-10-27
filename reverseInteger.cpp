f97a0106bd4f758a8b2cd5d599a245cbf6ca08dc
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-28 00:14:18 +0530

    Reverse of a num by leetcode

---
+++ reverseInteger.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int reverse(int x)
+{
+
+    int ans = 0;
+
+    while (x)
+    {
+
+        if ((ans < INT_MIN / 10) || (ans > INT_MAX / 10))
+        {
+            return 0;
+        }
+
+        ans = ans * 10 + x % 10;
+
+        x = x / 10;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin >> num;
+    cout <<"The reverse is : "<< reverse(num) << endl;
+};
