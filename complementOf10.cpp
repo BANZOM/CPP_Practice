0f7a1e25f43386c9c8046a1e9f55d7ca5012e93e
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-29 10:22:06 +0530

    sucessfully found the required num of places

--- complementOf10.cpp
+++ complementOf10.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 int main()
 {
-    unsigned long long int num;
-    cout<<"Enter a decimal number: ";
-    cin>>num;
+    unsigned long long int num, copy;
+    cout << "Enter a decimal number: ";
+    cin >> num;
 
+    copy = num;
+    int i = 0;
+    while (copy)
+    {
+        i++;
+        copy = copy >> 1;
+    }
+    cout<<i<<endl;
 }
