ae8e5e93cc9481d393210ada756a9bf91d178aeb
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-29 11:41:55 +0530

    File created to check if the input is power of 2

---
+++ isPowOf2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "**RETURN TRUE IF IS A POWER OF 2 ELSE RETURN FALSE**" << endl;
+    cout << "Enter a number: ";
+    cin >> num;
+}
