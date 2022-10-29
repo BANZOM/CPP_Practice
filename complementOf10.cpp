bb2e8c50d789906b9ebe64bf3c78f073c90c04d0
BANZOM <aditya.panwar.2208@gmail.com>
2022-10-29 10:41:10 +0530

    Complement Done

--- complementOf10.cpp
+++ complementOf10.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
-    unsigned long long int num, copy, mask;
+    unsigned long long int num, copy, mask, complement;
     cout << "Enter a decimal number: ";
     cin >> num;
 
     copy = num;
     int i = 0;
+
+    // For finding the number of
     while (copy)
     {
         i++;
@@ -19,5 +21,10 @@ int main()
     // cout<<i<<endl;
 
     mask = pow(2, i) - 1;
-    cout << mask << endl;
+    // cout << mask << endl;
+
+    // For complement of base 10
+    complement = (~num) & mask;
+    cout << "The Complement is: " << complement << endl;
+    return 0;
 }
