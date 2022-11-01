4713355046418137d83eaf86061f05ae468a1a50
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-01 18:38:09 +0530

    new file initialised

---
+++ switchCase1.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file switchCase1.cpp
+ * @author Aditya (you@domain.com)
+ * @brief Implementing Switch case statements to terminate the infinite loop
+ * @version 0.1
+ * @date 2022-11-01
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
