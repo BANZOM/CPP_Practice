3d8fc767f49c60e6c9fe852474331a05e97835be
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-16 23:07:01 +0530

    selectionSort fn (tested)

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -29,7 +29,7 @@ void selectionSort(int a[], int n)
     for (int i = 0; i < n - 1; i++)
     {
         int minIndex = i;
-        for (int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (a[j] < a[minIndex])
             {
@@ -37,7 +37,8 @@ void selectionSort(int a[], int n)
             }
         }
 
-        swap(&a[i], &a[minIndex]);
+        if (minIndex != i)
+            swap(&a[i], &a[minIndex]);
     }
 }
 
