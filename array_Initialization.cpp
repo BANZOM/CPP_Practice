880cb28fbbb889984b877976fc03ad0afab32662
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-18 15:55:51 +0530

    comment updated

--- array_Initialization.cpp
+++ array_Initialization.cpp
@@ -8,7 +8,7 @@ int main()
     int a[100] = {-1}; // set the first value to -1 rest all get 0;
     int b[100] = {0};  // set all the values to zero
     
-    fill_n(b,100,69); // set all the vaues to a particual num, here 69;
+    fill_n(b,100,69); // set all the vaues to a particual num, here 69; <algorithm> is needed
 
     for (int i = 0; i < 100; i++)
     {
