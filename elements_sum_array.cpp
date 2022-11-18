d20ee5885e68536e1cc76c29e1a0fbc4a9e2a449
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-18 18:52:07 +0530

    Read the array

---
+++ elements_sum_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int printSum(int a[], int n)
+{
+    int sum = 0;
+    return sum;
+}
+int main(int argc, char const *argv[])
+{
+    int len, arr[100];
+    cin >> len;
+
+    // read array
+    for (int i = 0; i < len; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The Sum of elements is: " << printSum(arr, len) << endl;
+
+    return 0;
+}
