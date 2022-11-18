b468812b8e62d522e4b6a151847dab6164f7e4f6
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-18 15:54:58 +0530

    fill_n(arr,size,num)

--- array_Initialization.cpp
+++ array_Initialization.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
     int a[100] = {-1}; // set the first value to -1 rest all get 0;
-    int b[100] = { 0 };
-    // fill(a[100],100,69);
+    int b[100] = {0};  // set all the values to zero
+    
+    fill_n(b,100,69); // set all the vaues to a particual num, here 69;
 
     for (int i = 0; i < 100; i++)
     {
