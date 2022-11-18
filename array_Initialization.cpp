e330a7847507de067d8f64a661ff5f6e47790ebe
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-18 15:51:23 +0530

    0 and -1

--- array_Initialization.cpp
+++ array_Initialization.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main()
 {
-    int a[10000] = {0};
+    int a[100] = {-1}; // set the first value to -1 rest all get 0;
+    int b[100] = { 0 };
+    // fill(a[100],100,69);
 
-    for (int i = 0; i < 10000;i++)
+    for (int i = 0; i < 100; i++)
     {
-        cout<<*(a+i)<<" ";
+        cout << *(b + i) << " ";
     }
-    
+    cout << endl;
 }
