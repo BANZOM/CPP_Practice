799f31606ab5e180b9a67bd15e1e75a3fc944452
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-20 01:50:00 +0530

    almost done

--- find_unique_element.cpp
+++ find_unique_element.cpp
@@ -16,9 +16,32 @@ void printArray(int a[], int n)
     }
     cout << endl;
 }
+int search(int s, int a[], int n)
+{
+    int found = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s == a[i])
+        {
+            found += 1;
+        }
+    }
+    if (found > 1)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
 int returnUnique(int a[], int n)
 {
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        int found = search(a[i], a, n);
+        if (found)
+            return a[i];
+    }
+    return -1;
 }
 int main(int argc, char const *argv[])
 {
@@ -27,6 +50,14 @@ int main(int argc, char const *argv[])
     cin >> size;
     readArray(arr, size);
 
-    cout << "The Unique element is: " << returnUnique(arr, size) << endl;
+    /**
+     * @brief
+     * a array contails n elements where n = m + 1
+     * m is the no. of dublicate pairs
+     * only 1 element is unique
+     * printing that element
+     */
+
+    cout << "The Unique element is(-1 if not found): " << returnUnique(arr, size) << endl;
     return 0;
 }
