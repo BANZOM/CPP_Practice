27ebe1d262c83a9b71b716f5a15ba90f420fad35
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-20 01:13:44 +0530

    added FN: print array

--- alternate_swap_array.cpp
+++ alternate_swap_array.cpp
@@ -8,12 +8,23 @@ void readArray(int a[], int n)
         cin >> a[i];
     }
 }
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
 int main(int argc, char const *argv[])
 {
     int n, arr[100];
     cout << "Enter the no. of elements: ";
     cin >> n;
     readArray(arr, n);
+
+    cout << "Before Alternate swap: ";
+    printArray(arr, n);
 
     return 0;
 }
