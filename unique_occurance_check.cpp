9474e7c0219b52e27053cb4f16d7239c54ffaa94
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-19 20:48:00 +0530

    Update unique_occurance_check.cpp

--- unique_occurance_check.cpp
+++ unique_occurance_check.cpp
@@ -2,3 +2,23 @@
 
 // arr = [1,2,2,1,1,3]
 // The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+// This is the function copied from leetcode codeeditor
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+        unordered_map<int,int> mp;
+        unordered_set<int> set;
+
+        for(auto num:arr)
+            mp[num]++;
+        
+        for(auto it:mp){
+            int freq = it.second;
+            set.insert(freq);
+        }
+        
+        return mp.size() == set.size() ;
+    }
+};
