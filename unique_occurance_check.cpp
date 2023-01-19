7ee9781e8bcdd03741bfafbeeed84cf4c0b957cc
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-19 20:47:00 +0530

    Update unique_occurance_check.cpp

--- unique_occurance_check.cpp
+++ unique_occurance_check.cpp
@@ -1,1 +1,4 @@
 // Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+// arr = [1,2,2,1,1,3]
+// The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
