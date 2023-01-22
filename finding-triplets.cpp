41f4150754ab99d22721a901fb2728d5afc73df0
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-22 09:19:15 +0530

    Added: explanation

--- finding-triplets.cpp
+++ finding-triplets.cpp
@@ -9,4 +9,6 @@ Sample Input:
 Sample Output:
 5 5 2
 
+Explanation:
+5 5 2 is the only triplet that adds up to 15. Note that the order of the output doesn’t matter, so 5 2 5 or 2 5 5 is also acceptable.
 */
