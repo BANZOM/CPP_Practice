ab70cb12a0f696a0f06c471125d227cee2abd620
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-27 19:05:31 +0530

    Update pivotIndex.cpp

--- pivotIndex.cpp
+++ pivotIndex.cpp
@@ -8,3 +8,7 @@
 // Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 // Right sum = nums[4] + nums[5] = 5 + 6 = 11
 
+// Input: nums = [1,2,3]
+// Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
