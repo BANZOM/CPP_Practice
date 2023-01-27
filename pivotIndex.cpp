06b91dfb72e7ca32454b4a575e37136c4a8f0154
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-27 19:05:50 +0530

    Update pivotIndex.cpp

--- pivotIndex.cpp
+++ pivotIndex.cpp
@@ -12,3 +12,27 @@
 // Output: -1
 // Explanation:
 // There is no index that satisfies the conditions in the problem statement.
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int len = nums.size();
+        if(len==0){
+            return -1;
+        }        
+        int totalSum=0,leftSum=0;
+
+        for(int i=0; i<len;i++)
+            totalSum+=nums[i];
+        
+        for(int i=0; i<len; i++){
+            if(leftSum==(totalSum-nums[i]))
+                return i;
+            else{
+                leftSum+=nums[i];
+                totalSum-=nums[i];
+            }
+        }
+        return -1;
+    }
+};
