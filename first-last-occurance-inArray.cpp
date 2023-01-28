82222e2b7eb817ed4ad3daa3c26cdf48005f2f1c
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-28 11:10:53 +0530

    Formatted: minor bugs improvement

--- first-last-occurance-inArray.cpp
+++ first-last-occurance-inArray.cpp
@@ -6,39 +6,56 @@
 // 4 7
 
 #include <bits/stdc++.h>
+
+int firstOccurance(vector<int> &arr, int n, int k) {
+  int first = -1;
+  int start = 0;
+  int end = n-1;
+  int mid = start + (end-start)/2;
+  while(start<=end){
+    if(arr[mid]==k) {
+      first = mid;
+      end = mid - 1;
+    }
+
+    else if (arr[mid]<k)
+      start = mid + 1;
+    
+    else
+      end = mid -1;
+    
+    mid = start + (end-start)/2;
+  }
+  return first;
+}
+
+int lastOccurance(vector<int> &arr, int n, int k) {
+  int last = -1;
+  int start = 0;
+  int end = n-1;
+  int mid = start + (end-start)/2;
+  while(start<=end){
+    if(arr[mid]==k) {
+      last = mid;
+      start = mid + 1;
+    }
+
+    else if (arr[mid]<k)
+      start = mid + 1;
+    
+    else
+      end = mid -1;
+    
+    mid = start + (end-start)/2;
+  }
+  return last;
+}
 pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k) {
-  int low = 0, high = n - 1;
-  int x = -1, y = -1;
 
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
+  pair<int, int> p;
 
-    if (arr[mid] == k) {
-      x = mid;
-      high = mid - 1;
-    } else if (k > arr[mid])
-      low = mid + 1;
-    else
-      high = mid - 1;
-  }
+  p.first = firstOccurance(arr, n, k);
+  p.second = lastOccurance(arr, n, k);
 
-  //   for (int i = x; i < n; i++) {
-  //     if (arr[i] != k)
-  //       break;
-  //     y = i;
-  //   }
-  low = 0, high = n - 1;
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
-
-    if (arr[mid] == k) {
-      y = mid;
-      low = mid + 1;
-    } else if (k > arr[mid])
-      low = mid + 1;
-    else
-      high = mid - 1;
-  }
-
-  return make_pair(x, y);
+  return p;
 }
