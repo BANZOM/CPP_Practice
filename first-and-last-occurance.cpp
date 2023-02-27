a352fb83000154f5b71855f62d8c203ebf415d7b
BANZOM <aditya.panwar.2208@gmail.com>
2023-02-27 16:45:58 +0530

    input elements

---
+++ first-and-last-occurance.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,arr[1000];
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    return 0;
+}
