73bb0fcc95370deb9d2dc206072d8e24a013e4fb
BANZOM <aditya.panwar.2208@gmail.com>
2023-02-27 16:53:17 +0530

    changes

--- first-and-last-occurance.cpp
+++ first-and-last-occurance.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int firstOccr(int arr[], int left, int right, int key)
+{
+
+    return -1;
+}
+int lastOccr(int arr[], int left, int right, int key)
+{
+    return -1;
+}
 int main()
 {
-    int n,arr[1000];
+    int n, arr[1000], key;
     cout << "Enter number of elements: ";
     cin >> n;
     cout << "Enter the elements: ";
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    
+    cout << "Enter the element to search: ";
+    cin >> key;
+    cout << "First occurance is " << firstOccr(arr, 0, n - 1, key) << ", Last occurance is " << lastOccr(arr, 0, n - 1, key) << endl;
+
     return 0;
 }
