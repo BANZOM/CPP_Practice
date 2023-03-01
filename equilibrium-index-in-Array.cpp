7e19cca4e645052c55399fccf316c809dce0318a
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-01 14:11:33 +0530

    Function Completed

--- equilibrium-index-in-Array.cpp
+++ equilibrium-index-in-Array.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int findEquilibrium(int arr[], int n)
 {
-    
+    int leftSum[n], rightSum[n];
+    int temp = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        leftSum[i] = temp;
+        temp += arr[i];
+    }
+
+    temp = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        rightSum[i] = temp;
+        temp += arr[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(leftSum[i] == rightSum[i])
+            return i;
+    }
+
+    return -1;
 }
 
 int main()
