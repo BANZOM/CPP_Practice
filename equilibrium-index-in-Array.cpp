462f9b79f2e43036ac9c022757afa8fb2ee0379b
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-01 17:06:41 +0530

    Added: more approach

--- equilibrium-index-in-Array.cpp
+++ equilibrium-index-in-Array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int findEquilibrium(int arr[], int n)
+int findEquilibrium_Approach1(int arr[], int n)
 {
     int leftSum[n], rightSum[n];
     int temp = 0;
@@ -19,12 +19,32 @@ int findEquilibrium(int arr[], int n)
         temp += arr[i];
     }
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if(leftSum[i] == rightSum[i])
+        if (leftSum[i] == rightSum[i])
             return i;
     }
 
+    return -1;
+}
+
+int findEquilibrium_Approach2(int arr[], int n)
+{
+    int totalSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += arr[i];
+    }
+
+    int temp = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        totalSum -= arr[i];
+        if (totalSum == temp)
+            return i;
+        temp += arr[i];
+    }
     return -1;
 }
 
@@ -39,6 +59,8 @@ int main()
         cin >> arr[i];
     }
 
-    cout << "The Equilibrium element index is : " << findEquilibrium(arr, n) << endl;
+    cout << "The Equilibrium element index (Approach 1) is : " << findEquilibrium_Approach1(arr, n) << endl;
+    cout << "The Equilibrium element index (Approach 2) is : " << findEquilibrium_Approach2(arr, n) << endl;
+
     return 0;
 }
