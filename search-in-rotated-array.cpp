f8046361bbc3945db3cd4922f4278768ce367a8f
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-02 16:52:04 +0530

    added function

--- search-in-rotated-array.cpp
+++ search-in-rotated-array.cpp
@@ -3,23 +3,43 @@ using namespace std;
 
 int findPivot(int *arr, int n)
 {
-    int left = 0, right = n-1, mid;
-    while (left<=right)
+    int left = 0, right = n - 1, mid;
+    while (left < right)
     {
-        mid = left + (right-left)/2;
+        mid = left + (right - left) / 2;
 
-        if(arr[mid] >= arr[0])
+        if (arr[mid] >= arr[0])
             left = mid + 1;
-        else 
+        else
             right = mid;
     }
 
     return mid;
 }
 
+int binarySearch(int *arr, int left, int right, int key)
+{
+    int mid = left + (right - left) / 2;
+
+    while (left <= right)
+    {
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] > key)
+            right = mid - 1;
+        else
+            left = mid + 1;
+
+        mid = left + (right - left) / 2;
+    }
+    return mid;
+}
+
 int findIndex(int *arr, int n, int key)
 {
-    int pivot = findPivot(arr,n);
+    int pivot = findPivot(arr, n);
+
+    if ()
 }
 
 int main()
