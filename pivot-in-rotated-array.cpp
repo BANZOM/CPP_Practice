6f3bb0ddf9ef409d5f61ded8131791272fd426b1
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-02 13:32:44 +0530

    Update file

--- pivot-in-rotated-array.cpp
+++ pivot-in-rotated-array.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int findPivot(int *arr, int n)
+{
+    
+}
+
 int main()
 {
     int arr[] = {5, 9, 2, 3, 4};
     int n = 5;
+
+    cout << "The Pivot index is " << findPivot(arr, n) << endl;
     return 0;
 }
