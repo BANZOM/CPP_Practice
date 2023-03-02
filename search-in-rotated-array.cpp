753fa3c3ba43f7cf99dbd9b865c27b8e5076d908
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-02 16:29:09 +0530

    New Program: Searching element in rotated array

---
+++ search-in-rotated-array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int findIndex(int *arr, int n, int key)
+{
+    
+}
+int main()
+{
+    int arr[] = {5, 8, 13, 2, 4};
+    int n = 5;
+    int key = 4;
+
+    cout << "The element is at index : " << findIndex(arr, n, key) << endl;
+    return 0;
+}
