f8de8a736b6de8a7a49119cd81b53079ea5da696
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-02 17:04:14 +0530

    Completed

--- search-in-rotated-array.cpp
+++ search-in-rotated-array.cpp
@@ -19,34 +19,34 @@ int findPivot(int *arr, int n)
 
 int binarySearch(int *arr, int left, int right, int key)
 {
-    int mid = left + (right - left) / 2;
-
     while (left <= right)
     {
+        int mid = left + (right - left) / 2;
         if (arr[mid] == key)
             return mid;
         if (arr[mid] > key)
             right = mid - 1;
         else
             left = mid + 1;
-
-        mid = left + (right - left) / 2;
     }
-    return mid;
+    return -1;
 }
 
 int findIndex(int *arr, int n, int key)
 {
     int pivot = findPivot(arr, n);
 
-    
+    if (key >= arr[pivot] && key < arr[0])
+        return binarySearch(arr, pivot, n - 1, key);
+    else
+        return binarySearch(arr, 0, pivot - 1, key);
 }
 
 int main()
 {
     int arr[] = {5, 8, 13, 2, 4};
     int n = 5;
-    int key = 4;
+    int key = 14;
 
     cout << "The element is at index : " << findIndex(arr, n, key) << endl;
     return 0;
