cf65dd83188a4342d271adf6a65e196b95f0347d
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-03 07:23:42 +0530

    Modified

--- square-root.cpp
+++ square-root.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+double findRoot(int n)
+{
+    int start = 0, end = n / 2 + 1;
+    int root = 0;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (mid * mid <= n)
+        {
+            root = mid;
+            start = mid + 1;
+        }
+        else
+            end = mid - 1;
+    }
+
+    return root;
+}
+
 int main()
 {
-    int n = 67;
+    int n = 50;
+
+    cout << "The Root of " << n << " is " << findRoot(n) << endl;
+
+    return 0;
 }
