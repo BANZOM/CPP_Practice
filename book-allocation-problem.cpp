2c31b19b6b5fa877cfc0e9f8f4aa24103cf4c093
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-11 11:49:54 +0530

    File Updated

--- book-allocation-problem.cpp
+++ book-allocation-problem.cpp
@@ -9,14 +9,38 @@ Output:
 Return that minimum possible number.
 */
 
-int isPossibleSolution(int *A, int n, int S, int mid)
+bool isPossibleSolution(int *A, int n, int S, int mid)
 {
+    int pages = 0, studentCount = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (pages + A[i] <= mid)
+            pages += A[i];
+
+        else
+        {
+            studentCount++;
+
+            if (studentCount > S || A[i] > mid)
+                return 0;
+
+            pages = A[i];
+        }
+        if (studentCount > S) // when books left to be allocated
+            return 0;
+    }
+    return 1;
 }
 
 int allocateBook(int *A, int n, int S)
 {
     int ans = -1;
     int left = 0, right = 0;
+
+    // no. of student is more than no. of books
+    if (S > n)
+        return -1;
 
     for (int i = 0; i < n; i++)
         right += A[i]; // this is the search space
