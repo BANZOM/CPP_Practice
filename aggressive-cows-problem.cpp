31bdbae4084d70d10092e2becb93a9dd43f53e8f
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-11 17:47:36 +0530

    File Created

---
+++ aggressive-cows-problem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/*
+Input:
+The first argument given is the integer array A. that is the position of stall.
+The second argument given is the integer B. that is the number of cows.
+
+Output:
+Return that minimum distance between any two cows is maximum
+*/
