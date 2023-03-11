b2a9934029b70ae401f4e6b7b9e7c8ab0e5370c7
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-11 11:11:11 +0530

    Update File

--- book-allocation-problem.cpp
+++ book-allocation-problem.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
+/*
+Input:
+The first argument given is the integer array A.
+The second argument given is the integer B.
 
+Output:
+Return that minimum possible number.
+*/
+
+int main()
+{
+     
+}
