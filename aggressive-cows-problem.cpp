7e79ecce33ff7dfefd8b3fd00c34044219c0646e
BANZOM <aditya.panwar.2208@gmail.com>
2023-03-11 17:49:57 +0530

    File Updated

--- aggressive-cows-problem.cpp
+++ aggressive-cows-problem.cpp
@@ -8,3 +8,25 @@ The second argument given is the integer B. that is the number of cows.
 Output:
 Return that minimum distance between any two cows is maximum
 */
+
+int aggressiveCows(int *A, int n, int C)
+{
+    int ans = -1;
+    return ans;
+}
+
+int main()
+{
+    int A[100];
+    int n, C;
+
+    cin >> n >> C;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    cout << "Answer = " << aggressiveCows(A, n, C) << endl;
+
+    return 0;
+}
